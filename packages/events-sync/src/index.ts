ece9668e360f3675f9a49334987559f14dd1de34
Philipp Langer <philipplanger@yahoo.com>
2026-09-06 09:41:34 +0200

    chore(events): keep external adapters out of manual pilot

--- packages/events-sync/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./contracts.js";
-export * from "./eventfrog-adapter.js";
-export * from "./normalization.js";
-export * from "./pending-adapters.js";
-export * from "./repository.js";
-export * from "./sync.js";
